341bf538a79bad7ca78ced2ff657bb3f9ce6fac1
mitchldtn <mitchldtn@gmail.com>
2026-04-29 01:42:41 -0500

    add some padding

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,7 +1,7 @@
 const Intro = () => {
   const IntroHeader = () => {
     return (
-      <div className="md:flex flex-col text-4xl leading-relaxed">
+      <div className="md:flex flex-col text-4xl leading-relaxed pb-8">
         <span>
           Engineer<span className="text-redish">.</span> Maker<span className="text-redish">.</span> Builder<span className="text-redish">.</span>
         </span>
@@ -11,8 +11,8 @@ const Intro = () => {
 
   const IntroText = () => {
     return (
-      <div className="mt-20 w-full sm:w-3/5">
-        <span className="w-full h-full items-center mb-10">
+      <div className="mt-10 w-full sm:w-3/5">
+        <span className="w-full h-full items-center mb-10 text-xl">
           Originally from Turks & Caicos islands, now based in Austin, Texas.
           I build scalable distributed systems, hardware prototypes and everything in between.
         </span>
